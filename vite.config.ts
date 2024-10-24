cb61a693ad024203323219d4da083a3e74179fb1
shreyash ingle <109937258+ShreyashIngle@users.noreply.github.com>
2024-10-24 16:20:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,10 +13,8 @@ export default defineConfig({
     },
   },
   server: {
-    // port: 3000, // You can change this to your preferred port
+    port: 3000, // You can change this to your preferred port
     open: true, // Automatically open the app in the browser on server start
-    // Proxy configuration (optional)
-    // Uncomment this section if you need to proxy requests to your backend server
     proxy: {
       '/api': {
         target: 'https://invoice-generator-kz6p.onrender.com', // Your backend server
